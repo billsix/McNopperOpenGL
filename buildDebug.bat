1435840556d771917f5a5a9fb556f8e2f2eb087e
William Emerison Six <billsix@gmail.com>
2021-11-05 18:35:27 -0400

    build debug on Windows

--- buildDebug.bat
+++ buildDebug.bat
@@ -2,7 +2,7 @@ mkdir debugBuild
 mkdir debugBuildInstall
 cd debugBuild
 REM create the visual studio solution and project files
-cmake -DCMAKE_INSTALL_PREFIX=..\debugBuildInstall ..\
+cmake -DCMAKE_INSTALL_PREFIX=..\debugBuildInstall -DCMAKE_BUILD_TYPE=Debug ..\
 cmake --build . --target ALL_BUILD --config Debug
 cmake --build . --target INSTALL 
 cd ..
